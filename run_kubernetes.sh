a778099d3fb495a5f854affd4eec5a1168105154
Ubuntu <ubuntu@ip-172-31-28-253.ec2.internal>
2021-08-23 16:00:14 +0000

    Updated files needed to pass projects

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,20 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=adenicole/flask-sklearn
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run myapp \
+    --image=$dockerpath \
+    --port=80 --labels app=myapp
 
 # Step 3:
 # List kubernetes pods
-
+kubectl get pods
 # Step 4:
 # Forward the container port to a host
+kubectl port-forward myapp 8000:80
+# Log container outputs
+kubectl logs --follow myapp
 
